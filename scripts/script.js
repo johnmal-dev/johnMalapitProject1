c1734e6cec568887a94bf04ee6e534aa3289804c
John Malapit <johncmalapit@gmail.com>
2022-12-16 14:36:38 -0500

    implement image modal

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,31 @@ let imgSrc;
 // add event listeners to all images
 images.forEach((img) => {
   img.addEventListener('click', (e) => {
+    console.log(e.target);
     imgSrc = e.target.src;
     console.log(imgSrc);
-    imgModal();
+    imgModal(imgSrc);
   });
 });
 
 console.log(images);
 
 // CREATING THE MODAL
-let imgModal = () => {
+let imgModal = (src) => {
   const modal = document.createElement('div');
   modal.setAttribute('class', 'modal');
+  // add modal to the main section
   document.querySelector('main').append(modal);
+  // add image to modal
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', src);
+  // creating the close button
+  const closeBtn = document.createElement('i');
+  closeBtn.setAttribute('class', 'fas fa-times closeBtn');
+  closeBtn.onclick = () => {
+    modal.remove();
+  };
+  modal.append(newImage, closeBtn);
 };
+
+//
